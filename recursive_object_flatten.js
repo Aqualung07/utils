fbb516c27395f1a00293bb27e1fe2740b8ca5476
Pablo Silva Hernández <pablosh97@gmail.com>
2023-01-17 15:40:14 +0100

    Added recursive object flatten

---
+++ recursive_object_flatten.js
@@ -0,0 +1,27 @@
+function flattenObject(obj) {
+    let flattenObj = {};
+    if (typeof obj !== 'object') return;
+    Object.keys(obj).forEach(key => {
+        if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+            return flattenObj = {...flattenObj, ...flattenObject(obj[key])};
+        }
+        flattenObj = {...flattenObj, [key]: obj[key]};
+    })
+    return flattenObj;
+}
+
+let cat = {
+    name: 'Jack',
+    surname: 'Miau',
+    address: {
+        country: 'Meowland',
+        state: 'State of Meows',
+        house: {
+            doorNumber: '1',
+            floorNumber: '2'
+        },
+        array: [1,2,3,4,5,6]
+    }
+};
+
+console.log(flattenObject(cat));
